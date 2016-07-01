1eedd2aa2416cf0069fe92cc00879dda89e62adb
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-07-01 23:24:00 +0200

    indents

--- templates/acasa-template.php
+++ templates/acasa-template.php
@@ -73,32 +73,32 @@
 </section>
 
 
-	<div class = "row intro-row first-row">
-		<div class = "col s12">
-		    <div class = "row center">
-		      <div class = "col s12">
-		        <h1><?php echoFieldValueOrDefault('intro_titlu'); ?></h1>
-		      </div>
-		    </div>
-		    <div class = "row center">
-		      <div class = "col s6 offset-s3">
-		      	<h2><?php echoFieldValueOrDefault('intro_subtitlu'); ?></h2>
-		      </div>
-		    </div>
-		    <div class = "row center">
-		      <div class = "col s6 offset-s3">
-		        <?php echoFieldValueOrDefault('intro_continut'); ?>
-		      </div>
-		    </div>
-		    <div class = "row center">
-		      <div class = "col s12">
-		        <a id = "afla-cine-suntem-button" class="intro-action-button waves-effect waves-light btn" href="<?php echoFieldValueOrDefault('intro_button_link'); ?>">
-		          <span><?php echoFieldValueOrDefault('intro_button_text'); ?></span>
-		        </a>
-		      </div>
-		    </div>
-		</div>
-	</div>
+  <div class = "row intro-row first-row">
+    <div class = "col s12">
+        <div class = "row center">
+          <div class = "col s12">
+            <h1><?php echoFieldValueOrDefault('intro_titlu'); ?></h1>
+          </div>
+        </div>
+        <div class = "row center">
+          <div class = "col s6 offset-s3">
+            <h2><?php echoFieldValueOrDefault('intro_subtitlu'); ?></h2>
+          </div>
+        </div>
+        <div class = "row center">
+          <div class = "col s6 offset-s3">
+            <?php echoFieldValueOrDefault('intro_continut'); ?>
+          </div>
+        </div>
+        <div class = "row center">
+          <div class = "col s12">
+            <a id = "afla-cine-suntem-button" class="intro-action-button waves-effect waves-light btn" href="<?php echoFieldValueOrDefault('intro_button_link'); ?>">
+              <span><?php echoFieldValueOrDefault('intro_button_text'); ?></span>
+            </a>
+          </div>
+        </div>
+    </div>
+  </div>
     <div class = "row center">
       <div class = "col s12">
         <h2><?php echoFieldValueOrDefault('proiecte_titlu'); ?></h2>
@@ -106,67 +106,67 @@
     </div>
     <div class = "row center">
       <div class = "col s6 offset-s3">
-      	<a id = "vezi-toate-proiectele-link" href = "<?php echoFieldValueOrDefault('proiecte_link'); ?>">
-      		<?php echoFieldValueOrDefault('proiecte_link_text'); ?>
-      	</a>
+        <a id = "vezi-toate-proiectele-link" href = "<?php echoFieldValueOrDefault('proiecte_link'); ?>">
+          <?php echoFieldValueOrDefault('proiecte_link_text'); ?>
+        </a>
       </div>
     </div>
 
 
-	<?php
-		$args = array(
-	      'post_type'     	=> 'proiect',
-	      'post_status'   	=> 'publish',
-	      'orderby'       	=> 'date',
-	      'posts_per_page'  => 4
-	    );
+  <?php
+    $args = array(
+        'post_type'       => 'proiect',
+        'post_status'     => 'publish',
+        'orderby'         => 'date',
+        'posts_per_page'  => 4
+      );
 
       $query = new WP_Query($args);
 
       if($query->have_posts()) {
-            	?>
+              ?>
         <div class = "row project-list-row">
            <?php
-            	while($query->have_posts()) {
-              	  $query->the_post();
+              while($query->have_posts()) {
+                  $query->the_post();
 
-              	  $thumb_url 		= "";
-              	  $category 		= get_the_category()[0];
-                  $titluCF 		    = get_post_meta($post->ID, 'titlu',true);
-				  $organizatieCF 	= get_post_meta($post->ID, 'organizatie',true);
+                  $thumb_url    = "";
+                  $category     = get_the_category()[0];
+                  $titluCF        = get_post_meta($post->ID, 'titlu',true);
+          $organizatieCF  = get_post_meta($post->ID, 'organizatie',true);
 
-                  $titlu 	   = $titluCF !== NULL && $titluCF !== ""  ? $titluCF : get_the_title();
-              	  $organizatie = $organizatieCF !== NULL ? $organizatieCF : "";
+                  $titlu     = $titluCF !== NULL && $titluCF !== ""  ? $titluCF : get_the_title();
+                  $organizatie = $organizatieCF !== NULL ? $organizatieCF : "";
 
-                  $content 	= $titlu  . " // " . $organizatie;
-                  $class 	= $category->slug . '-card' . ' ' . $category->slug . '-class';
+                  $content  = $titlu  . " // " . $organizatie;
+                  $class  = $category->slug . '-card' . ' ' . $category->slug . '-class';
 
-              	if (has_post_thumbnail()) {
-              		$thumb_id  = get_post_thumbnail_id();
+                if (has_post_thumbnail()) {
+                  $thumb_id  = get_post_thumbnail_id();
                     $thumb_url = wp_get_attachment_image_src($thumb_id, "full")[0];
-              	}
+                }
 
-              		?>
-              	<div class="col s12 m6 l3">
-	              	<div class="card project-card <?php echo $class; ?>" data-category = "<?php echo $category->slug; ?>">
-		              	<a href="<?php the_permalink(); ?>">
-      						<div class="card-image">
-      						    <img src="<?php echo $thumb_url;?>">
-      						    <span class="card-title"></span>
-      						</div>
-      						<div class="card-content">
-      						    <p class = "center"><?php echo $content;?></p>
-      						</div>
-      					</a>
-      				</div>
-      			</div>
-          	<?php } ?>
-			</div>
-		<?php
+                  ?>
+                <div class="col s12 m6 l3">
+                  <div class="card project-card <?php echo $class; ?>" data-category = "<?php echo $category->slug; ?>">
+                    <a href="<?php the_permalink(); ?>">
+                  <div class="card-image">
+                      <img src="<?php echo $thumb_url;?>">
+                      <span class="card-title"></span>
+                  </div>
+                  <div class="card-content">
+                      <p class = "center"><?php echo $content;?></p>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <?php } ?>
+      </div>
+    <?php
 
-		wp_reset_query();
+    wp_reset_query();
 
-	} ?>
+  } ?>
 
 
 
@@ -194,14 +194,14 @@
 
 
                   foreach ($entities as $entity) {
-                  	$titlu 		 = repeaterFieldValueOrDefault("titlu", $entity);
-                  	$descriere 	 = repeaterFieldValueOrDefault("descriere", $entity);
-                  	$poza 		 = repeaterFieldValueOrDefault("poza", $entity);
-                  	if($poza === null || $poza === "") {
-          				$poza = $defaultEntityPic;
-          			}
+                    $titlu     = repeaterFieldValueOrDefault("titlu", $entity);
+                    $descriere   = repeaterFieldValueOrDefault("descriere", $entity);
+                    $poza      = repeaterFieldValueOrDefault("poza", $entity);
+                    if($poza === null || $poza === "") {
+                  $poza = $defaultEntityPic;
+                }
 
-                  	?>
+                    ?>
                     <li>
                       <img src="<?php echo $poza;?>">
                       <p class = "entitate-titlu"><?php echo $titlu;?></p>
--- templates/proiecte-template.php
+++ templates/proiecte-template.php
@@ -3,47 +3,47 @@
 <div class="row">
     <div class="col s12">
         <div class = "frame-container">
-        	<span>
-        		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam feugiat erat, eu placerat sem faucibus eu. Nullam eget faucibus eros, non auctor nisl. Nullam eleifend sodales risus, id porta erat. Curabitur dictum elementum ex. Aliquam orci mauris, viverra sit amet laoreet eu, scelerisque ac dui. Aenean dignissim erat nulla, facilisis maximus mauris varius a. Praesent ornare vulputate bibendum. Vivamus convallis luctus gravida.
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquam feugiat erat, eu placerat sem faucibus eu. Nullam eget faucibus eros, non auctor nisl. Nullam eleifend sodales risus, id porta erat. Curabitur dictum elementum ex. Aliquam orci mauris, viverra sit amet laoreet eu, scelerisque ac dui. Aenean dignissim erat nulla, facilisis maximus mauris varius a. Praesent ornare vulputate bibendum. Vivamus convallis luctus gravida.
 
-				    Nullam tellus odio, tincidunt sed justo et, maximus elementum odio. Quisque vel congue leo. Praesent laoreet, felis ac accumsan efficitur, sapien lectus facilisis tellus, sit amet ultricies nisi dui ut lorem. Vivamus vitae magna cursus, euismod ligula ut, mattis ligula. Aenean eleifend tellus at justo pellentesque, et volutpat justo commodo. Morbi quis ullamcorper felis. Morbi vitae purus finibus, viverra justo eu, lacinia quam.
-        	</span>
+            Nullam tellus odio, tincidunt sed justo et, maximus elementum odio. Quisque vel congue leo. Praesent laoreet, felis ac accumsan efficitur, sapien lectus facilisis tellus, sit amet ultricies nisi dui ut lorem. Vivamus vitae magna cursus, euismod ligula ut, mattis ligula. Aenean eleifend tellus at justo pellentesque, et volutpat justo commodo. Morbi quis ullamcorper felis. Morbi vitae purus finibus, viverra justo eu, lacinia quam.
+          </span>
         </div>
     </div>
-</div> 
+</div>
 <div class="row">
     <div class="col s12">
         <div class = "frame-container">
-        	<div class="row center proiecte-filtre-row">
-				<?php
-					$filters 		= getCategoryActiveFilters();
-					$columnClass 	= "s" . (12 / count($filters));
-					foreach ($filters as $filter) { 
-						$class 	= $filter->slug . '-class';
-						$cbID  	= $filter->slug . '-cb';
-					?>
+          <div class="row center proiecte-filtre-row">
+        <?php
+          $filters    = getCategoryActiveFilters();
+          $columnClass  = "s" . (12 / count($filters));
+          foreach ($filters as $filter) {
+            $class  = $filter->slug . '-class';
+            $cbID   = $filter->slug . '-cb';
+          ?>
 
-						<div class="col <?php echo $columnClass;?>">
-							 <a class="<?php echo $class;?> filter-button waves-effect waves-light btn" href="#!">
-			           <span><?php echo $filter->name;?></span>
-			         </a>
-			                
-			         <input type="checkbox" class="filled-in project-filter-cb" checked="checked" id="<?php echo $cbID;?>" data-category = "<?php echo $filter->slug; ?>" />
-			         <label class = "project-filter-cb-label" for="<?php echo $cbID;?>"></label>
-						</div>
+            <div class="col <?php echo $columnClass;?>">
+               <a class="<?php echo $class;?> filter-button waves-effect waves-light btn" href="#!">
+                 <span><?php echo $filter->name;?></span>
+               </a>
+
+               <input type="checkbox" class="filled-in project-filter-cb" checked="checked" id="<?php echo $cbID;?>" data-category = "<?php echo $filter->slug; ?>" />
+               <label class = "project-filter-cb-label" for="<?php echo $cbID;?>"></label>
+            </div>
 
 
-					<?php
-					}
-				?>
-			</div>
+          <?php
+          }
+        ?>
+      </div>
         </div>
     </div>
 </div>
 
 
-	<?php
-		    $args = array(
+  <?php
+        $args = array(
               'post_type'      => 'proiect',
               'post_status'    => 'publish',
               'orderby'        => 'date',
@@ -53,55 +53,55 @@
       $query = new WP_Query($args);
 
       if($query->have_posts()) {
-            	?>
+              ?>
         <div class = "row">
-					<div class="col s12">
-						<ul class = "project-list">
-            	<?php
-            	$index = 0;
+          <div class="col s12">
+            <ul class = "project-list">
+              <?php
+              $index = 0;
 
-            	while($query->have_posts()) {
-              		$query->the_post();
+              while($query->have_posts()) {
+                  $query->the_post();
 
-              		$thumb_url 		  = "";
-              		$category 		  = get_the_category()[0];
-                  $titluCF 		    = get_post_meta($post->ID, 'titlu',true);
-					        $organizatieCF 	= get_post_meta($post->ID, 'organizatie',true);
-                    
-                  $titlu 		   = $titluCF !== NULL && $titluCF !== ""  ? $titluCF : get_the_title();
-              		$organizatie = $organizatieCF !== NULL ? $organizatieCF : "";
+                  $thumb_url      = "";
+                  $category       = get_the_category()[0];
+                  $titluCF        = get_post_meta($post->ID, 'titlu',true);
+                  $organizatieCF  = get_post_meta($post->ID, 'organizatie',true);
 
-                  $content 	= $titlu  . " // " . $organizatie;                    
-                  $class 		= $category->slug . '-card' . ' ' . $category->slug . '-class';
+                  $titlu       = $titluCF !== NULL && $titluCF !== ""  ? $titluCF : get_the_title();
+                  $organizatie = $organizatieCF !== NULL ? $organizatieCF : "";
 
-              		if (has_post_thumbnail()) {
-              		    $thumb_id  = get_post_thumbnail_id();
+                  $content  = $titlu  . " // " . $organizatie;
+                  $class    = $category->slug . '-card' . ' ' . $category->slug . '-class';
+
+                  if (has_post_thumbnail()) {
+                      $thumb_id  = get_post_thumbnail_id();
                       $thumb_url = wp_get_attachment_image_src($thumb_id, "full")[0];
-              		}
+                  }
 
-              		?>
-              		<li class = "center">              		
-	              	   <div class="card project-card <?php echo $class; ?>" data-category = "<?php echo $category->slug; ?>">
-		              	   	<a href="<?php the_permalink(); ?>">
-      						       <div class="card-image">
-      						         <img src="<?php echo $thumb_url;?>">
-      						         <span class="card-title"></span>
-      						       </div>
-      						       <div class="card-content">
-      						         <p class = "center"><?php echo $content;?></p>
-      						       </div>
-      						    </a>
-      					    </div>              		
-              		</li>
-              		<?php
+                  ?>
+                  <li class = "center">
+                     <div class="card project-card <?php echo $class; ?>" data-category = "<?php echo $category->slug; ?>">
+                        <a href="<?php the_permalink(); ?>">
+                         <div class="card-image">
+                           <img src="<?php echo $thumb_url;?>">
+                           <span class="card-title"></span>
+                         </div>
+                         <div class="card-content">
+                           <p class = "center"><?php echo $content;?></p>
+                         </div>
+                      </a>
+                    </div>
+                  </li>
+                  <?php
 
-              		$index++;
-              	}
+                  $index++;
+                }
 
-              	?>
+                ?>
               </ul>
-			</div>
-		</div>
-		<?php } ?>
-		
+      </div>
+    </div>
+    <?php } ?>
+
 

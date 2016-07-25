85f50c0d4a2cc608e82d1122218c54d528cba756
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-07-25 13:33:44 +0200

    cum lucram columns

--- templates/cum-lucram-template.php
+++ templates/cum-lucram-template.php
@@ -1,26 +1,15 @@
 <?php /* Template Name: Cum lucram */ ?>
 
-    <div class = "row first-row center">
-      <div class = "col s12">
-        <h1><?php echoFieldValueOrDefault('cum_lucram_titlu'); ?></h1>
-      </div>
-    </div>
-    <div class = "row center">
-      <div class = "col s6 offset-s3">
+<section class="block block-hero block-content wrap container">
+  <div class="hero">
+    <div class="hero-content content row">
+      <h1 class="small-offset-1 small-10 medium-offset-0 medium-12 columns">
+        <?php echoFieldValueOrDefault('cum_lucram_titlu'); ?>
+      </h1>
+      <div class="small-offset-1 small-10 medium-6 columns">
         <?php echoFieldValueOrDefault('cum_lucram_continut'); ?>
       </div>
-    </div>
-
-
-<!--
-<section class="block wrap container">
-  <div class="content row">
-    <div class="small-12 columns">
-      <?php //get_template_part('templates/page', 'header'); ?>
-
-
-
+      <div class="small-12 columns"></div>
     </div>
   </div>
 </section>
--->

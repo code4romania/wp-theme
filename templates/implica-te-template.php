3787861fd78dd98e594105146de1b3bd7d5e305d
Razvan Pavel <razvan.e.pavel@gmail.com>
2017-04-01 19:52:27 +0300

    get involved fields

--- templates/implica-te-template.php
+++ templates/implica-te-template.php
@@ -5,9 +5,10 @@
     <div class="hero-content content row">
       <?php get_template_part('templates/page', 'header'); ?>
 
+
+
       <div class="small-offset-1 small-10 medium-offset-2 medium-8 columns">
-        <p>Comunitățile active creează o societate mai puternică, care se poate dezvolta mai repede și mai eficient. Pe termen lung, ele pot duce la o Românie mai bună, în care am vrea cu toții să trăim.</p>
-        <p>Dacă vrei să contribui și tu, vino alături de noi.<br/>Câteva ore din timpul tău, know-how sau o donație oricât de mică, toate ne pot ajuta.</p>
+        <?php echo the_field('implica-te_hero'); ?>
       </div>
 
     </div>
@@ -16,96 +17,55 @@
 
 <section class="block wrap container">
   <div class="content row" data-equalizer data-equalize-on="small" data-equalize-on-stack="false" data-equalize-by-row="true">
-    <div class="small-12 medium-offset-1 medium-10 large-5 columns">
-      <div class="involve involve-action involve-time" data-equalizer-watch id="timp">
-        <h2>
-          <i class="svg-icons svg-inverted svg-time"></i>
-          <span>Timp</span>
-        </h2>
-        <p>Poate vrei să te implici cât iți permite timpul pentru o cauză socială. Avem nevoie de skillurile tale: programare, comunicare, design, organizare evenimente, cercetare, analiză de date și altele la care poate nici nu ne-am gândit.</p>
-        <p>Completează formularul din link ca să știm mai multe despre tine. Te vom contacta noi apoi pentru pașii următori.</p>
-        <p>
-          <a href="https://www.surveymonkey.com/r/C6MZJJJ" class="button large underline inverted" target="_blank">Hai cu noi!</a>
-        </p>
-      </div>
-    </div>
-    <div class="small-12 medium-offset-1 medium-10 large-offset-0 large-5 columns">
-      <div class="involve involve-action involve-money" data-equalizer-watch id="bani">
-        <h2>
-          <i class="svg-icons svg-donate svg-money"></i>
-          <span>Bani</span>
-        </h2>
-        <p>Proiectul Code for Romania se bazează pe voluntariat. Cu toate acestea, întreţinerea platformei, domeniile web și hostingul, participarea în anumite proiecte, lucrurile administrative etc. implică anumite costuri.</p>
-        <p>Dacă îți plac <a href="/proiecte/">proiectele noastre</a>, poți investi în ele printr-o donație. Orice contribuţie, oricât de mică, este utilă şi foarte apreciată. Fiecare donație ajută proiectele să meargă înainte și ne permite să ne investim energia în lucrurile cu adevărat importante: dezvoltarea proiectelor și creșterea comunității.</p>
-        <p>
-          <a href="/doneaza/" class="button large underline button-donate inverted">Ajută-ne să continuăm!</a>
-        </p>
-      </div>
-    </div>
-    <div class="small-offset-1 small-10 medium-offset-2 medium-8 large-offset-1 large-5 columns">
-      <div class="involve" data-equalizer-watch id="companii">
-        <h2>
-          <i class="svg-icons svg-companies"></i>
-          <span>Companii</span>
-        </h2>
-        <p>O societate mai puternică contribuie la un mediu de business mai sănătos.</p>
-        <p>Dacă vrei să susții proiecte care vor schimba încet-încet România, ne-ar plăcea să ne prezentăm și să vedem cum putem lucra împreună.<br/>
-        <p>Ne poți susține astfel:</p>
-        <ul class="list">
-          <li>Donează linii de cod (pentru companiile de software development)</li>
-          <li>Susține-ne organizatoric (ex. găzduieste întâlnirile voluntarilor)</li>
-          <li>Adoptă un proiect acoperind parțial sau total cheltuielile aferente</li>
-          <li>Oferă premii pentru voluntarii noștri</li>
-          <li>Donează produse (ex. software sau hardware)</li>
-          <li>Prezintă-ne angajaților tăi (poate vor să devină voluntari)</li>
-          <li>Oferă sponsorizari de orice alt fel.</li>
-        </ul>
-        <p>
-          <a href="mailto:<?php echo antispambot('parteneriate@code4.ro'); ?>" class="button large underline">Scrie-ne un e-mail</a>
-        </p>
-      </div>
-    </div>
-    <div class="small-offset-1 small-10 medium-offset-2 medium-8 large-offset-0 large-5 columns">
-      <div class="involve" data-equalizer-watch id="institutii">
-        <h2>
-          <i class="svg-icons svg-goverment"></i>
-          <span>Instituții</span>
-        </h2>
-        <p>Vă invităm să dezvoltăm împreună instrumente digitale pentru cetăţeni, care să:</p>
-        <ul class="list">
-          <li>Rezolve probleme punctuale ale oamenilor</li>
-          <li>Informeze cetățenii în mod transparent și rapid și să le faciliteze accesul la datele publice</li>
-          <li>Implice activ cetățenii in procesul de guvernare</li>
-        </ul>
-        <p>Pe termen lung, credem că atât instituțiile, cât și cetățenii vor beneficia de îmbunătățirea nivelului culturii digitale în sectorul public: calitatea interacțiunii cetățean-instituție va crește, iar folosirea resurselor va fi optimizată.</p>
-        <p>
-          <a href="https://code4romania.typeform.com/to/fDNLY4" target="_blank" class="button large underline">Propune un proiect</a>
-        </p>
-      </div>
-    </div>
-    <div class="small-offset-1 small-10 medium-offset-2 medium-8 large-offset-1 large-5 columns">
-      <div class="involve" data-equalizer-watch id="ong">
-        <h2>
-          <i class="svg-icons svg-organisations"></i>
-          <span>ONG-uri</span>
-        </h2>
-        <p>Știm că vă confruntați zi de zi cu o mulțime de provocări, de la gestionarea activităților cu voluntarii până la găsirea celei mai bune soluții pentru beneficiarii proiectelor voastre. Mai știm și că aveți o grămadă de idei bune, care rămân doar pe hârtie, din lipsă de resurse.</p>
-        <p>
-          <a href="https://code4romania.typeform.com/to/fDNLY4" target="_blank" class="button large underline">Propune un proiect</a>
-        </p>
-      </div>
-    </div>
-    <div class="small-offset-1 small-10 medium-offset-2 medium-8 large-offset-0 large-5 columns">
-      <div class="involve" data-equalizer-watch>
-        <h2>
-          <i class="svg-icons svg-universities"></i>
-          <span>Universități</span>
-        </h2>
-        <p>Un parteneriat între Code For Romania și mediul academic este o modalitate foarte atractivă de implicare a studenților în activități extracuriculare. Putem dezvolta împreună proiecte de practică pentru studenți sau putem colabora la proiecte de cercetare.</p>
-        <p>
-          <a href="mailto:<?php echo antispambot('parteneriate@code4.ro'); ?>" class="button large underline">Vrem să vă cunoaștem</a>
-        </p>
-      </div>
-    </div>
+      <?php
+        $implicari = get_field("implicari");
+
+        for($index = 0; $index < count($implicari); $index++) {
+
+          if($index < count($implicari)) {
+
+            $implicare = $implicari[$index];
+            $titlu = repeaterFieldValueOrDefault("implica_titlu", $implicare);
+            $continut = repeaterFieldValueOrDefault("implica_continut", $implicare);
+            $icon = repeaterFieldValueOrDefault("implica_icon", $implicare);
+            $tema = repeaterFieldValueOrDefault("implica_tema", $implicare);
+            $ctaURL = repeaterFieldValueOrDefault("implica_call_to_action_url", $implicare);
+            $ctaLabel = repeaterFieldValueOrDefault("implica_call_to_action_string", $implicare);
+            $buttonInverted = "";
+            $iconInverted = "";
+
+            $emailRx = '/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})/';
+
+            if(preg_match($emailRx, $ctaURL, $email_is)) {
+              $ctaURL = antispambot($ctaURL);
+            }
+
+            if($tema == "action" || $tema == "special") {
+              $buttonInverted = " inverted";
+              $iconInverted = " svg-inverted";
+            }
+
+            $hasAction = $tema !== "default" ? " involve-action" : "";
+            $hasDonate = $tema === "special" ? " svg-donate" : "";
+
+            $html = "<div class='small-12 medium-6 columns'>" .
+                      "<div class='involve " . $hasAction . " involve-" . $icon ."' data-equalizer-watch id='" . $icon ."'>" .
+                        "<h2>" .
+                          "<i class='svg-icons " . $iconInverted . $hasDonate . " svg-" . $icon ."'></i>" .
+                          "<span>" . $titlu ."</span>" .
+                        "</h2>" .
+                        "<div class='involve-content'>" .
+                          $continut .
+                        "</div>" .
+                        "<p>" .
+                          "<a href='" . $ctaURL . "' class='button large underline" . $buttonInverted . "'>" . $ctaLabel . "</a>" .
+                        "</p>" .
+                      " </div>" .
+                    " </div>";
+
+            echo $html;
+          }
+        }
+      ?>
   </div>
 </section>

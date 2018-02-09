1238d17b284032c87fea976bb39683f769fd60d3
Andrei Ioniță <hi@andrei.io>
2018-02-09 21:58:01 +0200

    Add content fields

--- templates/donate-card.php
+++ templates/donate-card.php
@@ -7,7 +7,7 @@
   <div class="donate-hero">
     <h1><?php echo the_title(); ?></h1>
     <p class="donate-copy">
-      <?php the_field('donate_content_body'); ?>
+      <?php the_field('doneaza_intro'); ?>
     </p>
     <form action="" method="post" class="donate-form">
       <div class="donate-step">
@@ -16,7 +16,7 @@
             <span class="donate-count">1</span>
           </div>
           <div class="media-object-section">
-            <div class="donate-label js-label-donate"><?php _e('Lorem ipsum dolor sit amet, consectetur adipisicing elit.'); ?></div>
+            <div class="donate-label js-label-donate"><?php the_field('doneaza_step_1'); ?></div>
             <div class="donate-options row">
               <?php
                 $donations_config = apply_filters('donations_gateway_get_config');
@@ -40,7 +40,7 @@
               <div class="donate-recurrent small-12 columns">
                 <input name="donate[recurrent]" value="0" type="hidden">
                 <input name="donate[recurrent]" value="1" type="checkbox" id="donate-monthly-recurrence" >
-                <label for="donate-monthly-recurrence">Vreau sa va ajut in fiecare luna.</label>
+                <label for="donate-monthly-recurrence"><?php the_field('doneaza_recurrent'); ?></label>
               </div>
             </div>
           </div>
@@ -53,27 +53,27 @@
           </div>
           <div class="media-object-section">
             <div>
-              <div class="donate-label js-label-donate" data-value="50"> By coming together, we can solve the challenges we face today. If you want to contribute with your resources.</div>
               <div class="row">
                 <div class="small-12 medium-7 columns">
                   <div class="donate-person">
-                    <input type="text" name="donate[name]" placeholder="Numele tau complet" required>
                   </div>
+              <div class="donate-label js-label-donate"><?php the_field('doneaza_step_2'); ?></div>
+                    <input type="text" name="donate[name]" placeholder="<?php the_field('doneaza_label_nume'); ?>" required>
                 </div>
                 <div class="small-12 medium-7 columns">
                   <div class="donate-person">
-                    <input type="email" name="donate[email]" placeholder="Adresa ta de e-mail" required>
                   </div>
                 </div>
                 <div class="small-12 medium-5 columns">
                   <input name="donate[currency]" type="hidden" value="<?php echo $donations_config['currency']['code']; ?>">
                   <input type="submit" class="button donate-button" value="Donează">
+                    <input type="email" name="donate[email]" placeholder="<?php the_field('doneaza_label_e-mail'); ?>" required>
                 </div>
               </div>
               <p class="donate-secure">
                 <i class="fa fa-lock"></i>
-                Vei fi redirectionat catre Mobipay pentru finalizarea tranzactiei indata ce dai click click.</p>
             </div>
+              <?php the_field('doneaza_security'); ?>
           </div>
         </div>
       </div>

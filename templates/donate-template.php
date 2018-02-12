3ce1f84369d3fafffa9f6b4af3023ae1e1b5851c
Razvan Pavel <razvan.e.pavel@gmail.com>
2018-02-12 16:43:02 +0200

    Update donate template

--- templates/donate-template.php
+++ templates/donate-template.php
@@ -2,50 +2,75 @@
 <section class="block block-hero block-content block-donate wrap container">
   <div class="hero">
     <div class="content row">
-
       <?php get_template_part('templates/donate', 'card'); ?>
-
-      <div class="small-offset-1 small-10 medium-offset-2 medium-8 columns">
-        <?php the_field('doneaza_hero'); ?>
-      </div>
-
-      <div class="small-offset-1 small-10 columns actions">
-        <a href="https://www.patreon.com/bePatron?u=3907223&redirect_uri=http%3A%2F%2Fwww.code4.ro%2Fmultumim%2F" class="button large underline donate-cta" target="_blank">
-          <img src="<?php the_field('doneaza_call_to_action_icon'); ?>" alt="<?php the_field('doneaza_call_to_action_string'); ?>" class="button-ui" aria-hidden="true">
-          <?php the_field('doneaza_call_to_action_string'); ?>
-        </a>
-      </div>
     </div>
   </div>
 </section>
 
-<section class="block block-hero block-content wrap container">
-  <div class="hero-content content row">
+<section class="block block-content">
+  <div class="row">
+    <div class="columns small-12 large-6">
+      <div class="boxer is-donate">
+        <h2><?php the_field('doneaza_options_title'); ?></h2>
+        <div class="boxer-content">
 
-    <div class="small-offset-1 small-10 medium-offset-2 medium-8 columns">
+          <div class="donate-handler">
+            <div class="donate-handlerTitle clearfix">
+              <div class="donate-handlerLabel pull-left">prin</div>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" class="donate-handlerAction pull-left">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="U4QBTHU9J5HUS">
+                <input type="image" src="<?php bloginfo('template_url'); ?>/dist/images/paypal.png" name="submit" alt="Doneaza prin PayPal" class="img-button">
+                <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+              </form>
+            </div>
+            <div class="doante-handlerContent">
+              <p><?php the_field('doneaza_option_paypal_content'); ?></p>
+            </div>
+          </div>
 
-      <div class="donate row">
-        <div class="small-12 columns dontate-intro">
-          <?php the_field('doneaza_intro'); ?>
+          <?php
+            $donateOptions = get_field('doneaza_options');
+
+            for($index = 0; $index < count($donateOptions); $index++) {
+              $donateOption = $donateOptions[$index];
+
+              $optionImage = repeaterFieldValueOrDefault('doneaza_option_image', $donateOption);
+              $optionLink = repeaterFieldValueOrDefault('doneaza_option_link', $donateOption);
+              $optionContent = repeaterFieldValueOrDefault('doneaza_option_content', $donateOption);
+          ?>
+            <div class="donate-handler">
+              <div class="donate-handlerTitle clearfix">
+                <div class="donate-handlerLabel pull-left">prin</div>
+                <a href="<?php echo $optionLink;?>" target="_blank" class="donate-handlerAction pull-left">
+                  <img src="<?php echo $optionImage;?>" alt="" />
+                </a>
+              </div>
+              <div class="doante-handlerContent">
+                <?php echo $optionContent; ?>
+              </div>
+            </div>
+          <?php } ?>
+
         </div>
-
-        <div class="small-12 large-6 columns">
-          <div class="donate-option">
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" class="dontate-cta">
-              <input type="hidden" name="cmd" value="_s-xclick">
-              <input type="hidden" name="hosted_button_id" value="U4QBTHU9J5HUS">
-              <input type="image" src="<?php the_field('doneaza_secondary_image'); ?>" name="submit" alt="Doneaza prin PayPal" class="img-button">
-              <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-            </form>
-            <div class="donate-info">
-              <?php the_field('doneaza_secondary_content'); ?>
+      </div>
+    </div>
+    <div class="columns small-12 large-6">
+      <div class="row">
+        <div class="columns small-12 medium-6 large-12">
+          <div class="boxer is-donate">
+            <h2><?php the_field('doneaza_wire_title'); ?></h2>
+            <div class="boxer-content">
+              <?php the_field('doneaza_wire_content'); ?>
             </div>
           </div>
         </div>
-        <div class="small-12 large-6 columns banks">
-          <h2><?php the_field('doneaza_tertiary_title'); ?></h2>
-          <div class="donate-content">
-            <?php the_field('doneaza_tertiary_content'); ?>
+        <div class="columns small-12 medium-6 large-12">
+          <div class="boxer is-donate">
+            <h2><?php the_field('doneaza_other_title'); ?></h2>
+            <div class="boxer-content">
+              <?php the_field('doneaza_other_content'); ?>
+            </div>
           </div>
         </div>
       </div>
